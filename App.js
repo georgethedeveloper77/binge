4aaff315909bfab97b2b21874815936ff714271b
georgethedeveloper77 <georgekarani77@gmail.com>
2020-07-11 21:41:35 +0300

    const firebase = require("firebase"); require("firebase/firestore"); Upload Photos and Posts with Cloud Firestore

--- App.js
+++ App.js
@@ -112,7 +112,6 @@ const AppContainer = createStackNavigator(
   {
     mode: "modal",
     headerMode: "none",
-    initialRouteName: "postModal",
   }
 );
 
--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -9,34 +9,69 @@ import {
   TextInput,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
-import Contants from "expo-constants";
+import Constants from "expo-constants";
 import * as Permissions from "expo-permissions";
 import Fire from "../Fire";
+import * as ImagePicker from "expo-image-picker";
+
+const firebase = require("firebase");
+require("firebase/firestore");
 
 export default class PostScreen extends React.Component {
   state = {
-    text: ""
-    Image: null
+    text: "",
+    Image: null,
+  };
+
+  componentDidMount() {
+    this.getPhotoPermission();
   }
 
-  getPhotoPermission = async() => {
-    if ('Contants.platform.android') { //.ios
-      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+  getPhotoPermission = async () => {
+    if ("Contants.platform.android") {
+      //.ios
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
 
       if (status != "granted") {
-        alert("we need permissinonn to access your camera roll")
+        alert("We need permissinonn to access your camera roll");
       }
-
     }
-  }
+  };
+
+  handlePost = () => {
+    Fire.shared
+      .addPost({
+        text: this.state.text.trim(),
+        localUri: this.state.image,
+      })
+      .then((ref) => {
+        this.setState({ text: "", image: null });
+        this.props.navigation.goBack();
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
+
+  pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypesOptions.Images,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.header}>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
             <Ionicons name="md-arrow-back" size={24} color="#D8D9D8"></Ionicons>
           </TouchableOpacity>
-          <TouchableOpacity>
+          <TouchableOpacity onPress={this.handlePost}>
             <Text style={{ fontWeight: "500" }}>Post</Text>
           </TouchableOpacity>
         </View>
@@ -52,11 +87,20 @@ export default class PostScreen extends React.Component {
             numberOfLines={4}
             style={{ flex: 1 }}
             placeholder="Want to share Something"
+            onChangeText={(text) => this.setState({ text })}
+            value={this.state.text}
           ></TextInput>
         </View>
-        <TouchableOpacity style={styles.photo}>
+        <TouchableOpacity style={styles.photo} onPress={this.pickImage}>
           <Ionicons name="hd-camera" size={32} color="#D8D9DZB"></Ionicons>
         </TouchableOpacity>
+
+        <View style={{ marginHorizontal: 32, marginTop: 32, height: 150 }}>
+          <Image
+            source={{ uri: this.state.image }}
+            style={{ width: "100%", height: "100%" }}
+          ></Image>
+        </View>
       </SafeAreaView>
     );
   }
