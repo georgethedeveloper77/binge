85b85e1a54948a9bc43ba09f78e24158c19e4462
georgethedeveloper77 <georgekarani77@gmail.com>
2020-07-13 18:24:33 +0300

    firebase Keys

--- App.js
+++ App.js
@@ -16,17 +16,9 @@ import NotificationScreen from "./screens/NotificationScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 
 import * as firebase from "firebase";
+import { FirebaseKeys } from "./config";
 
-var firebaseConfig = {
-  apiKey: "AIzaSyB5WCTAlZnM3ARrejR6v8MFJIyA5U1ajfo",
-  authDomain: "binge-5fe63.firebaseapp.com",
-  databaseURL: "https://binge-5fe63.firebaseio.com",
-  projectId: "binge-5fe63",
-  storageBucket: "binge-5fe63.appspot.com",
-  messagingSenderId: "385686415497",
-  appId: "1:385686415497:web:bc4d90b75fa88ed3437e58",
-  measurementId: "G-EKX0HX4HFH",
-};
+var firebaseConfig = FirebaseKeys
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
--- Fire.js
+++ Fire.js
@@ -1,20 +1,10 @@
 //import FirebaseKeys from "./config";
 import firebase from "firebase";
-
-var firebaseConfig = {
-  apiKey: "AIzaSyB5WCTAlZnM3ARrejR6v8MFJIyA5U1ajfo",
-  authDomain: "binge-5fe63.firebaseapp.com",
-  databaseURL: "https://binge-5fe63.firebaseio.com",
-  projectId: "binge-5fe63",
-  storageBucket: "binge-5fe63.appspot.com",
-  messagingSenderId: "385686415497",
-  appId: "1:385686415497:web:bc4d90b75fa88ed3437e58",
-  measurementId: "G-EKX0HX4HFH",
-};
+import { FirebaseKeys } from "./config";
 
 class Fire {
   constructor() {
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(FirebaseKeys);
   }
 
   addPost = async ({ text, localUri }) => {
