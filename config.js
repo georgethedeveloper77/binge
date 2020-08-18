5de1cf5c61aa375b790a5fe2e6b116e8c1024ca3
georgethedeveloper77 <georgekarani77@gmail.com>
2020-08-18 14:00:31 +0300

    config file

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 // Make sure to copy your keys from your firebase console!
 
 export default FirebaseKeys = {
-  apiKey: "AIzaSyB5WCTAlZnM3ARrejR6v8MFJIyA5U1ajfo",
-  authDomain: "binge-5fe63.firebaseapp.com",
-  databaseURL: "https://binge-5fe63.firebaseio.com",
-  projectId: "binge-5fe63",
-  storageBucket: "binge-5fe63.appspot.com",
-  messagingSenderId: "385686415497",
-  appId: "1:385686415497:web:bc4d90b75fa88ed3437e58",
-  measurementId: "G-EKX0HX4HFH",
+  apiKey: "AIzaSyCbV_iZuMfpK6ZLXMWOoD7GEgpk5nFid6U",
+  authDomain: "binge-951e7.firebaseapp.com",
+  databaseURL: "https://binge-951e7.firebaseio.com",
+  projectId: "binge-951e7",
+  storageBucket: "binge-951e7.appspot.com",
+  messagingSenderId: "747858778223",
+  appId: "1:747858778223:web:57233b35d88a40a4da159a",
+  measurementId: "G-4ZDM4XGS6B"
 };
