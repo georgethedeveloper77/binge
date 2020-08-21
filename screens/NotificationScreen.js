18d676710f5f8a8e424ca870669b9e32368cc324
georgethedeveloper77 <georgekarani77@gmail.com>
2020-08-21 04:55:58 +0300

    setup again

--- screens/NotificationScreen.js
+++ screens/NotificationScreen.js
@@ -2,19 +2,19 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 export default class NotificationScreen extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Notification Screen</Text>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Notification Screen</Text>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
+    container: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    }
 });
